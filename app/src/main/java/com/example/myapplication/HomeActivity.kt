a15f39473ab98dd521d4054fce9771ebf16bfb06
Salomon KAMARA <kamar_s6@samar57.cnam.fr>
2023-01-02 17:38:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val actionBar = supportActionBar
+        actionBar?.hide()
+
+        val button = findViewById<Button>(R.id.btnMain)
+        val button_favoris = findViewById<Button>(R.id.btnFavorite)
+
+        button.setOnClickListener{
+
+            startActivity(Intent(this, MainActivity::class.java).apply {
+
+            })
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
